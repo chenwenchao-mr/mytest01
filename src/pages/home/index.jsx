7aa003da7a61983ced41081ab9c99730df671e81
chenwenchaoMr <chenwenchao@hnlshm.com>
2023-10-26 14:01:01 +0800

    feat: 测试改动

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,7 +2,12 @@ import { memo, forwardRef } from "react";
 
 const Index = memo(
   forwardRef(() => {
-    return <div>react-router-v6 调试首页-测试自动化部署test</div>;
+    return (
+      <div>
+        <div>react-router-v6 调试首页-测试自动化部署test</div>
+        <div>main分支改动</div>
+      </div>
+    );
   })
 );
 
